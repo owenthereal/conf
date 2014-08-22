91bce1c91a4432ce9036038f6b5fa9bb66dfd39e
Martin Wernstål <m4rw3r@gmail.com>
2014-08-22 14:45:35 +0200

    Int(), Bool() now converts input strings properly and String() converts bools and ints

--- conf.go
+++ conf.go
@@ -1,5 +1,10 @@
 package conf
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Conf struct {
 	conf map[string]interface{}
 }
@@ -15,12 +20,14 @@ func (c *Conf) Get(key string) interface{} {
 func (c *Conf) Bool(key string) bool {
 	val := c.Get(key)
 	if val != nil {
-		v, ok := val.(bool)
-		if ok {
-			return v
+		switch val := val.(type) {
+		default:
+			return false
+		case string:
+			return strings.ToLower(strings.TrimSpace(val)) == "true"
+		case bool:
+			return val
 		}
-
-		return false
 	}
 
 	return false
@@ -32,6 +39,13 @@ func (c *Conf) Int(key string) int {
 		switch val := val.(type) {
 		default:
 			return 0
+		case string:
+			i, err := strconv.Atoi(val)
+			if err != nil {
+				return 0
+			}
+
+			return i
 		case float64:
 			return int(val)
 		case int:
@@ -45,12 +59,20 @@ func (c *Conf) Int(key string) int {
 func (c *Conf) String(key string) string {
 	val := c.Get(key)
 	if val != nil {
-		v, ok := val.(string)
-		if ok {
-			return v
+		switch val := val.(type) {
+		default:
+			return ""
+		case string:
+			return val
+		case int:
+			return strconv.Itoa(val)
+		case bool:
+			if val {
+				return "true"
+			} else {
+				return "false"
+			}
 		}
-
-		return ""
 	}
 
 	return ""
